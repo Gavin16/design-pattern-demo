8fce1e768cd3d8dffd017a8d6ce65fdd1a10d9c2
Minsky <mingorun@163.com>
2018-12-09 09:38:18 +0800

    Initial commit

---
+++ src/singleton/Singleton1.java
@@ -0,0 +1,17 @@
+package singleton;
+
+/**
+ * 单例模式
+ *
+ * 类型:饿汉模式; 在类加载时创建实例; 线程安全的
+ */
+public class Singleton1 {
+
+    private static Singleton1 instance = new Singleton1();
+
+    private Singleton1(){}
+
+    public static Singleton1 getInstance(){
+        return instance;
+    }
+}
---
+++ src/singleton/Singleton2.java
@@ -0,0 +1,21 @@
+package singleton;
+
+/**
+ * 单例模式
+ *
+ * 类型：懒汉模式; 在被调用获取实例时才创建实例； 线程不安全
+ */
+public class Singleton2 {
+
+    private Singleton2(){}
+
+    private static Singleton2 instance;
+
+    public static Singleton2 getInstance(){
+        if(null == instance){
+            instance = new Singleton2();
+        }
+        return instance;
+    }
+
+}
